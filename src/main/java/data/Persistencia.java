0d4e09c2a4392ac4f97adc6cdc8eb8e2842ed464
Mar <ingmarsaave@gmail.com>
2026-04-19 22:36:59 -0300

    Vista de Menu agregada. Sin Inicializado de Datos

--- src/main/java/data/Persistencia.java
+++ src/main/java/data/Persistencia.java
@@ -15,28 +15,7 @@ public class Persistencia {
     private static ArrayList<Sucursal> sucursales = new ArrayList<>();
 
     // Esto se ejecuta apenas arranca el programa
-    static {
-        inicializarDatos();
-    }
-
-    private static void inicializarDatos() {
-        // 1. Creamos un responsable (porque la clase Sucursal lo exige)
-        domain.Responsable resp = new domain.Responsable("Juan Perez", "20-12345678-9", "3814556677");
-
-        // 2. Creamos las sucursales (necesarias para poder asignar una al cargar un vehículo)
-        Sucursal s1 = new Sucursal("Sucursal Centro", "San Martín 123", "San Miguel de Tucumán", resp);
-        Sucursal s2 = new Sucursal("Sucursal Norte", "Av. Avellaneda 450", "San Miguel de Tucumán", resp);
-        
-        sucursales.add(s1);
-        sucursales.add(s2);
-
-        // 3. Dejamos las marcas creadas (opcional, para que el sistema tenga base de datos inicial)
-        Marca renault = new Marca("Renault", "Francia");
-        Marca ford = new Marca("Ford", "EEUU");
-        
-        // El punto 3.a pide eliminar los vehículos creados por código.
-        // La lista 'vehiculos' queda vacía para ser llenada solo desde la ventana 'Agregar'.
-    }
+    
 
     // Métodos para que el Controlador pueda sacar los datos
     public static ArrayList<Vehiculo> getVehiculos() {
